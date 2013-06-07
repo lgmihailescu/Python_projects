ae8cb08049140dec361823cfa8d5d8fc73c563d7
blidarescub <bogdan.blidarescu@gmail.com>
2013-06-07 17:57:54 +0300

    scanner new with scapy

---
+++ scapy/scanner.py
@@ -0,0 +1,8 @@
+from scapy.all import *
+#import scapy
+
+def scanner(pkt):
+    if IP in pkt and pkt[IP].proto == UDP:
+        return pkt.sprintf("%IP.src%:%TCP.sport% >>> %IP.dst%:%TCP.dport%")
+
+sniff(prn=scanner, filter="port 53", store=0)
