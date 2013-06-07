757d79e2ceb97fea51068789b673d557c0e5f25e
blidarescub <bogdan.blidarescu@gmail.com>
2013-06-07 19:01:10 +0300

    best

--- scapy/scanner.py
+++ scapy/scanner.py
@@ -6,5 +6,9 @@ def scanner(pkt):
         if UDP in pkt:
             dnsqr = pkt.getlayer(DNSQR)
             return pkt.sprintf("%IP.src%:%UDP.sport% >>> %IP.dst%:%UDP.dport% ") + dnsqr.sprintf("%qname% %qclass% %qtype%").replace("'","")
+try:
+    sniff(prn=scanner, filter=None, store=0)
+except KeyboardInterrupt:
+    exit(0)
 
-sniff(prn=scanner, filter=None, store=0)
+    
