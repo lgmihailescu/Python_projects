0847001881e6b3fab0e430b5f015d2219d6150e5
blidarescub <bogdan.blidarescu@gmail.com>
2013-06-07 18:44:03 +0300

    dnsqr

--- scapy/scanner.py
+++ scapy/scanner.py
@@ -4,6 +4,6 @@ from scapy.all import *
 def scanner(pkt):
     if IP in pkt:
         if UDP in pkt:
-            return pkt.sprintf("%IP.src%:%UDP.sport% >>> %IP.dst%:%UDP.dport% -- query %DNSQR.qtype%")
+            return pkt.sprintf("%IP.src%:%UDP.sport% >>> %IP.dst%:%UDP.dport% -- query %DNS(DNSQR.qtype)%")
 
 sniff(prn=scanner, filter=None, store=0)
