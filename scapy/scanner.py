dd1d119c4a2f71b03a125909c7c2d26b98ea0615
blidarescub <bogdan.blidarescu@gmail.com>
2013-06-07 17:59:47 +0300

    no proto

--- scapy/scanner.py
+++ scapy/scanner.py
@@ -2,7 +2,8 @@ from scapy.all import *
 #import scapy
 
 def scanner(pkt):
-    if IP in pkt and pkt[IP].proto == UDP:
+    if IP in pkt:
+        #and pkt[IP].proto == UDP:
         return pkt.sprintf("%IP.src%:%TCP.sport% >>> %IP.dst%:%TCP.dport%")
 
 sniff(prn=scanner, filter="port 53", store=0)
