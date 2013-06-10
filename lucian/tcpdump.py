ac41dd083801fdd67eda5598d2125258e97b649f
blidarescub <bogdan.blidarescu@gmail.com>
2013-06-10 17:47:39 +0300

    lucian

---
+++ lucian/tcpdump.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import datetime
+import argparse
+
+from scapy.all import *
+
+def scanner(pkt):
+    if pkt.haslayer(DNSQR):
+        q = pkt.getlayer(DNSQR)
+        tokens = ('.' + q.qname.rstrip('.')).rsplit('.', 2)
+        name, zone = tokens[0].lstrip('.'), '.'.join(tokens[-2:])
+        if zone.lower() not in filtered_zones:
+            return None
+        line = '{timestamp} {ipdst} {name} {zone} {type}'.format(
+            timestamp=datetime.datetime.utcnow().replace(microsecond=0),
+            ipdst=pkt.sprintf('%IP.dst%'),
+            name=name or '@',
+            zone=zone,
+            type=q.sprintf('%qtype%'),
+        )
+        if out_file:
+            out_file.write('%s\n' % line)
+            out_file.flush()
+            return None
+        else:
+            return line
+        # return pkt.sprintf("%IP.src%:%UDP.sport% >>> %IP.dst%:%UDP.dport% ") + dnsqr.sprintf("%qname% %qclass% %qtype%").replace("'","")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Capture DNS queries for specified zones')
+    parser.add_argument('zone', nargs='+', help='a DNS zone to be captured')
+    parser.add_argument('--output', '-o', help='write dump to file')
+
+    args = parser.parse_args()
+    print >> sys.stderr, 'Capturing DNS requests for zones %s' % args.zone
+    filtered_zones = set(z.lower() for z in args.zone)
+    try:
+        if args.output:
+            out_file = open(args.output, 'a')
+        else:
+            out_file = None
+        sniff(iface='eth0', filter='udp src port 53', prn=scanner, store=0)
+    except KeyboardInterrupt:
+        exit(0)
+    finally:
+        if out_file:
+            out_file.close()
